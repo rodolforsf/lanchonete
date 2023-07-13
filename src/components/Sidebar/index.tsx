8a9392d5cd31ba3e6cc6bcbdc8b5f1283838150d
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-07-13 06:57:05 -0300

    Construção parcial do Sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,24 @@
+import { Container } from "./styles"
+
+import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+
+export function Sidebar() {
+    return <Container>
+        <button type='button'>
+            <img src={menuImg} alt='Abrir e fechar o menu' />
+        </button>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        <BurguerIcon />
+                        <span>Hambúrgueres</span>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+    </Container>
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,12 @@
+import { Sidebar } from '../../components/Sidebar'
+
 import { Container } from './styles'
 
-const Container2 = () => {
-  return <h1>Container 2</h1>
-}
-
 export default function Main() {
-  return <Container2></Container2>
-  
+  return (
+    <Container>
+      <Sidebar />
+      <h1>Main</h1>
+    </Container>
+  )
 }
