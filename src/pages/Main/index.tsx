3b245cb6e6f9663933e4f583ffdb0550eb30ccd1
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-07-13 06:07:18 -0300

    "Atualização"

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,15 +1,10 @@
 import { Container } from './styles'
 
+const Container2 = () => {
+  return <h1>Container 2</h1>
+}
+
 export default function Main() {
-  return (
-    <Container>
-      <img
-        src='https://devsamurai-materials.s3.amazonaws.com/templates/avatar-gradient-dark.svg'
-        height='256'
-        alt='Dev Samurai'
-      />
-      <h1>Fala Samurai!</h1>
-      <p>Esse é o template typescript básico da Dev Samurai para React.</p>
-    </Container>
-  )
+  return <Container2></Container2>
+  
 }
