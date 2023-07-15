a5a8dfc519fbc3dc9a136c944f8a956a3b9ac1a2
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-07-15 10:45:38 -0300

    Head adicionado

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/Main'
-import Lanches  from './pages/Main/Lanches'
-import Pizzas from './pages/Main/Pizzas'
-import Bebidas from './pages/Main/Bebidas'
-import Sobremesas from './pages/Main/Sobremesas'
+import LanchesPagina  from './pages/Main/Lanches'
+import PizzasPagina from './pages/Main/Pizzas'
+import BebidasPagina from './pages/Main/Bebidas'
+import SobremesasPagina from './pages/Main/Sobremesas'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />} >
-        <Route path='/' element={<Lanches />} />
-        <Route path='/pizzas' element={<Pizzas />} />
-        <Route path='/bebidas' element={<Bebidas />} />
-        <Route path='/sobremesas' element={<Sobremesas />} />
+        <Route path='/' element={<LanchesPagina />} />
+        <Route path='/pizzas' element={<PizzasPagina />} />
+        <Route path='/bebidas' element={<BebidasPagina />} />
+        <Route path='/sobremesas' element={<SobremesasPagina />} />
       </Route>
     </Routes>
   )
